6657a420d082f14bd83ff34bd0bddb880cedc00a
Avadh Joshi <134098745+JoshiAvadh@users.noreply.github.com>
2026-02-23 14:38:57 -0800

    Time: 2 ms (2.5%), Space: 48.2 MB (71.5%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,32 +1,47 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int idx[] = {-1, -1};
+    public int findFirst(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
+        int idx = -1;
 
-        
         while(start <= end) {
-            int mid = start + (end - start)/2;
+            int mid = (start + end)/2;
 
-            if(nums[mid] == target) {
-                idx[0] = idx[1] = mid;
-                start = end = mid;
-                while(start >= 0 && nums[start] == target) {
-                    idx[0] = start;
-                    start--;
-                }
-                while(end < nums.length && nums[end] == target) {
-                    idx[1] = end;
-                    end++;
-                }
-                return idx;
-            }
-            if(nums[mid] < target) {
-                start = mid +1;
+            if(nums[mid] >= target) {
+                end = mid - 1;
             } else {
-                end = mid - 1; 
+                start = mid + 1;
             }
-        } 
+            if(nums[mid] == target) {
+                idx = mid;
+            }
+        }
         return idx;
+    }   
+
+    public int findLast(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int idx = -1;
+
+        while(start <= end) {
+            int mid = (start + end)/2;
+
+            if(nums[mid] <= target) {
+                start = start +1;
+            } else {
+                end = end - 1;
+            }
+            if(nums[mid] == target) {
+                idx = mid;
+            }
+        }
+        return idx;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int arr[] = new int[2];
+        arr[0] = findFirst(nums, target);
+        arr[1] = findLast(nums, target);
+        return arr;
     }
 }
