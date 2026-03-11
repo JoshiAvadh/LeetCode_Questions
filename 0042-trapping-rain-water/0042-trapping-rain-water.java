345e6890209f7624d041e6b47faa4f20b9672e7d
Avadh Joshi <134098745+JoshiAvadh@users.noreply.github.com>
2026-03-11 14:43:23 -0700

    Time: 0 ms (100%), Space: 47.4 MB (95.01%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,40 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int leftB[] = new int[n];
-        int rightB[] = new int[n];
+        // int n = height.length;
+        // int leftB[] = new int[n];
+        // int rightB[] = new int[n];
 
-        leftB[0] = height[0];
-        for(int i = 1; i < n; i++) {
-            leftB[i] = Math.max(leftB[i-1], height[i]);
-        }
+        // leftB[0] = height[0];
+        // for(int i = 1; i < n; i++) {
+        //     leftB[i] = Math.max(leftB[i-1], height[i]);
+        // }
 
-        rightB[n-1] = height[n-1];
-        for(int i = n-2; i >= 0; i--){
-            rightB[i] = Math.max(rightB[i+1], height[i]);
-        }
+        // rightB[n-1] = height[n-1];
+        // for(int i = n-2; i >= 0; i--){
+        //     rightB[i] = Math.max(rightB[i+1], height[i]);
+        // }
+        // int trapped = 0;
+
+        // for(int i = 0; i < n; i++) {
+        //     trapped += (Math.min(leftB[i], rightB[i]) - height[i]);
+        // }
+        // return trapped;
+
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = height[0];
+        int rightMax = height[height.length - 1];
         int trapped = 0;
-
-        for(int i = 0; i < n; i++) {
-            trapped += (Math.min(leftB[i], rightB[i]) - height[i]);
+        while(left < right) {
+            if(height[left] < height[right]) {
+                if(height[left] > leftMax) leftMax = height[left];
+                trapped += (leftMax - height[left]);
+                left++;
+            } else {
+                if(height[right] > rightMax) rightMax = height[right];
+                trapped += (rightMax - height[right]);
+                right--;
+            }
         }
         return trapped;
     }
