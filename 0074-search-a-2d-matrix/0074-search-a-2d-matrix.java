182793f6c6e339d8aa918bbf440de6b3feb2b0dc
Avadh Joshi <134098745+JoshiAvadh@users.noreply.github.com>
2026-09-01 16:21:31 -0700

    Time: 0 ms (100%), Space: 43.7 MB (91%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,68 +1,36 @@
-// class Solution {
-//     public boolean searchMatrix(int[][] matrix, int target) {
-//         int n = matrix.length;
-//         int m = matrix[0].length;
-
-//         int top = 0, bottom = m - 1;
-
-//         while(top <= bottom) {
-//             int row = (top + bottom)/2;
-
-//             if(target < matrix[row][0]) {
-//                 bottom = row - 1;
-//             } else if(target > matrix[row][m - 1]) {
-//                 top = row + 1;
-//             } else {
-//                 break;
-//             }
-//         }
-
-//         int row = (top + bottom)/2;
-//         int l = 0, r = matrix[0].length - 1;
-
-//         while(l <= r) {
-//             int mid = (l + r)/2;
-
-//             if(target > matrix[row][mid]) {
-//                 l = mid + 1;
-//             } else if(target < matrix[row][mid]) {
-//                 r = mid - 1;
-//             }
-//             else {
-//                 return true;
-//             }
-//         }
-//         return false;
-//     }
-// }
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int top = 0;
-        int low = n - 1;
+        int top = 0, bottom = n - 1;
 
-        while (top <= low) {
-            int row = (top + low) / 2;
+        while(top <= bottom) {
+            int row = (top + bottom)/2;
 
-            if (matrix[row][m - 1] < target) {
+            if(target < matrix[row][0]) {
+                bottom = row - 1;
+            } else if(target > matrix[row][m - 1]) {
                 top = row + 1;
-            } else if (matrix[row][0] > target) {
-                low = row - 1;
             } else {
                 break;
             }
         }
 
+        int row = (top + bottom)/2;
         int l = 0, r = matrix[0].length - 1;
-        int row = (top + low) / 2;
-        while (l <= r) {
-            int mid = (l + r) / 2;
 
-            if (target > matrix[row][mid]) l = mid + 1;
-            else if(target < matrix[row][mid]) r = mid - 1;
-            else return true;
+        while(l <= r) {
+            int mid = (l + r)/2;
+
+            if(target > matrix[row][mid]) {
+                l = mid + 1;
+            } else if(target < matrix[row][mid]) {
+                r = mid - 1;
+            }
+            else {
+                return true;
+            }
         }
         return false;
     }
